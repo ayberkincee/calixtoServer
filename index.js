1d4c6e4d6b284723da435cab01931b824dc875b7
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-06-25 13:21:17 -0500

    force false

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT_SERVER = process.env.PORT || 3003;
 // Syncing all the models at once.
 // conn.sync({ alter: true }).then(() => {
 // conn.sync({ force: true }).then(() => {
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT_SERVER, () => {
     console.log(`server listening at ${PORT_SERVER}`); // eslint-disable-line no-console
   });
