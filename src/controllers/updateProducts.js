2b39cc952c84921b549aa6ed5fc7151072e0b2e1
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-10-11 21:25:01 -0500

    updateProducts

--- src/controllers/updateProducts.js
+++ src/controllers/updateProducts.js
@@ -3,19 +3,20 @@
 const { Product, Category } = require("../db.js");
 
 async function updateProducts(req, res) {
+
   const products = req.body;
 
   try {
     products.forEach(async (p) => {
       const prodExist = await Product.findOne({ where: { id: p.id } });
-
       if (prodExist) {
         //console.log(`existe ${p.id}`);
-        p.descripcion ? (p.descripcion = p.descripcion.slice(0, 450)) : null;
+         p.descripcion ? (p.descripcion = p.descripcion.slice(0, 450)) : null;
+        
         const r = await Product.update(p, { where: { id: p.id } });
 
         const categoryId = Number(p.category?.slice(0, 4));
-        console.log("categoryId",categoryId);
+        // console.log("categoryId",categoryId);
         const category = p.category?.slice(7);
         // console.log(`${categoryId}: ${category}`);
         if (categoryId) {
