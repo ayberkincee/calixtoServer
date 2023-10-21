66128b741e73bf1649d49f6c3a1c48cef0a375e8
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-10-21 12:00:27 -0500

    update channels

--- src/controllers/updateProducts.js
+++ src/controllers/updateProducts.js
@@ -7,14 +7,12 @@ async function updateProducts(req, res) {
   const products = req.body;
 
   try {
-    products.forEach(async (p) => {
+    products.forEach(async (p) => { 
       const prodExist = await Product.findOne({ where: { id: p.id } });
       if (prodExist) {
-        //console.log(`existe ${p.id}`);
-         p.descripcion ? (p.descripcion = p.descripcion.slice(0, 450)) : null;
-        
-        const r = await Product.update(p, { where: { id: p.id } });
-
+        console.log("Este existe", prodExist.id);
+    
+        //creates or updates the category in categories table
         const categoryId = Number(p.category?.slice(0, 4));
         // console.log("categoryId",categoryId);
         const category = p.category?.slice(7);
@@ -31,6 +29,21 @@ async function updateProducts(req, res) {
             { where: { id: categoryId } }
           );
         }
+        
+        p.descripcion ? (p.descripcion = p.descripcion.slice(0, 450)) : null;
+        const r = await Product.update(p, { where: { id: p.id } });
+        
+        let newChannels = [];
+      
+        p.saludable === "SI" && newChannels.push(1);
+        p.autoservicio === "SI" && newChannels.push(2);
+        p.gym === "SI" && newChannels.push(3);
+        p.cafesCow === "SI" && newChannels.push(4);
+        p.horeca === "SI" && newChannels.push(5);
+        p.licoStores === "SI" && newChannels.push(6);
+        p.educacion === "SI" && newChannels.push(7);
+
+        prodExist.setChannels(newChannels);
       }
     });
     res.status(200).send("cargue de DB OK");
