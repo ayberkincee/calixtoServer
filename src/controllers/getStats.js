aaf2cea3153c528e19c2ed92f02a2a13a6e279ba
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-11-20 09:55:40 -0500

    stats graph bug fixed

--- src/controllers/getStats.js
+++ src/controllers/getStats.js
@@ -2,22 +2,23 @@ const { Stats } = require("../db");
 const { Op } = require("sequelize");
 
 const getStats = async (req, res) => {
+  console.log("served by getStats");
   const { range } = req.body;
   const day = 86400000;
   const today = new Date();
   const initDate = new Date(today - range * day);
-//   console.log("QQ", range, initDate, today);
+  // console.log("getStats9", range, initDate, today);
 
   try {
     const fullStats = await Stats.findAll({
       where: {
         createdAt: {
-          [Op.lt]: today,
+          [Op.lte]: today,
           [Op.gte]: initDate,
         },
       },
     });
-    // console.log("FS", fullStats);
+    // console.log("FS-getStats20", fullStats);
     res.status(200).json(fullStats);
   } catch (error) {
     res.status(400).send(`error consiguiendo las estadisticas ${error}`);
