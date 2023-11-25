39bcd35d4231a6ec93ab2f2d50b886a467ad101e
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-11-24 20:08:56 -0500

    x

--- src/controllers/getAllIcons.js
+++ src/controllers/getAllIcons.js
@@ -3,7 +3,6 @@ const {Icon} = require("../db")
 const getAllIcons = async (req, res) => {
     try {
         const allIcons = await Icon.findAll()
-        console.log(allIcons);
         res.status(200).json(allIcons)
     } catch (error) {
         res.status(400).json({error:error.message})
